83438414413e09d716f666f6181d939b21a7e885
DrLead <pascal.bollinger@t-online.de>
2021-09-18 17:05:20 +0200

    Ergänzung von Luft- und Bodenfeuchte

--- app.py
+++ app.py
@@ -15,9 +15,34 @@ def home():
         return "Debug Message"
     if (request.method == 'POST'): 
         req = request.json
-        if req['humidity'] > 40:
+        if req['temp'] > 22.5:
             return jsonify(req)
         else:
             return jsonify(req)
+
+@app.route('/api/humidity', methods=['GET', 'POST'])
+def home():
+    if (request.method == 'GET'):
+        return "Debug Message"
+    if (request.method == 'POST'): 
+        req = request.json
+        if req['humidity'] > 280:
+            return jsonify(req)
+        else:
+            return jsonify(req)
+
+@app.route('/api/soil_humidity', methods=['GET', 'POST'])
+def home():
+    if (request.method == 'GET'):
+        return "Debug Message"
+    if (request.method == 'POST'): 
+        req = request.json
+        if req['soil_humidity'] > 12:
+            return jsonify(req)
+        else:
+            return jsonify(req)
+
+
+
 if __name__ == "__main__":
     app.run()
