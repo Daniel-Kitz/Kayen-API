4c7ad027edb1aff0aabe5ae6d2e266a8d897900f
DrLead <pascal.bollinger@t-online.de>
2021-09-18 18:01:47 +0200

    add data csv's and response

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+from csv import writer
 from flask import Flask, request, jsonify
 from flask.templating import render_template
 
@@ -9,38 +10,49 @@ app.config["DEBUG"] = True
 def website():
     return render_template('index.html')
 
-@app.route('/api/temp', methods=['GET', 'POST'])
-def temp():
+@app.route('/api', methods=['GET', 'POST'])
+def data_ckeck():
     if (request.method == 'GET'):
         return "Debug Message"
-    if (request.method == 'POST'): 
-        req = request.json
-        if req['temp'] > 22.5:
-            return jsonify(req)
-        else:
-            return jsonify(req)
+    if (request.method == 'POST'):
 
-@app.route('/api/humidity', methods=['GET', 'POST'])
-def humidity():
-    if (request.method == 'GET'):
-        return "Debug Message"
-    if (request.method == 'POST'): 
-        req = request.json
-        if req['humidity'] > 280:
-            return jsonify(req)
-        else:
-            return jsonify(req)
+        Richtwert_Temperatur = 22.5
+        Richtwert_Luftfeuchte = 280
+        Richtwert_Bodenfeuchte = 12
 
-@app.route('/api/soil_humidity', methods=['GET', 'POST'])
-def soil_humidity():
-    if (request.method == 'GET'):
-        return "Debug Message"
-    if (request.method == 'POST'): 
         req = request.json
-        if req['soil_humidity'] > 12:
-            return jsonify(req)
+        with open('Daten/temperature.csv','a', newline='') as fd:
+            writer_object= writer(fd)
+            writer_object.writerow(str(req['temperature']))
+            fd.close()
+        with open('Daten/humidity.csv','a', newline='') as fd:
+            writer_object= writer(fd)
+            writer_object.writerow(str(req['humidity']))
+            fd.close()
+        with open('Daten/bodenfeuchte.csv','a', newline='') as fd:
+            writer_object= writer(fd)
+            writer_object.writerow(str(req['bodenfeuchte']))
+            fd.close()
+
+        response_liste = []
+        if req['temperature'] > Richtwert_Temperatur:
+            response_liste += ["Nicht Heizen"]
         else:
-            return jsonify(req)
+            response_liste += ["Heizen"]
+
+        if req['humidity'] > Richtwert_Luftfeuchte:
+            response_liste += ["Lüften"]
+        else: 
+            response_liste += ["Nicht Lüften"]
+        if req['bodenfeuchte'] > Richtwert_Bodenfeuchte:
+            response_liste += ["Nicht Bewässern"]
+        else:
+            response_liste += ["Bewässern"]
+        
+
+        res = {"response" : response_liste}
+        return jsonify(res)
+
 
 if __name__ == "__main__":
     app.run()
