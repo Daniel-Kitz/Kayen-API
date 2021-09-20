fd00a46d9f3996319ee06ef204d9c94092f7dbba
LordQuantumKeks <lordquantumkeks@gmail.com>
2021-09-20 18:44:40 +0200

    Added client js, added website, added api calls, reworked api, added charts, added chart.js dependency, added new string format, reworked data formating, light bugfixing, added css framework, removed unnecessary files

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header.js';
+import Chart from './components/Chart.js';
 
 function App() {
+
+  const [soil, setSoil] = useState([{}]);
+  const [humid, setHumid] = useState([{}]);
+  const [temp, setTemp] = useState([{}]);
+  useEffect(() => {
+    setInterval(() => {
+      fetch("/api/soil").then(
+        res => res.json()
+      ).then(
+        data => {
+          setHumid(data)
+          console.log(data)
+        }
+      )
+      fetch("/api/temp").then(
+        res => res.json()
+      ).then(
+        data => {
+          setTemp(data)
+          console.log(data)
+        }
+      )
+      fetch("/api/humid").then(
+        res => res.json()
+      ).then(
+        data => {
+          setSoil(data)
+          console.log(data)
+        }
+      )
+    }, 2000);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+    <Chart sensordata={soil} chartlabel={'Soild Humidity in %'} chartbordercolor={'rgba(255, 99, 132, 0.2)'} chartbgcolor={'rgb(255, 99, 132)'}/>
+    <Chart sensordata={temp} chartlabel={'Temperature in °C'} chartbordercolor={'rgba(132, 99, 255, 0.2)'} chartbgcolor={'rgb(32, 99, 120)'}/>
+    <Chart sensordata={humid} chartlabel={'Humidity in %'} chartbordercolor={'rgba(0, 99, 132, 0.2)'} chartbgcolor={'rgb(255, 255, 255)'}/>
+    </>
   );
 }
 
-export default App;
+export default App
---
+++ client/src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+function Header() {
+  return <Button variant="contained">Hello World</Button>;
+}
+
+export default Header
